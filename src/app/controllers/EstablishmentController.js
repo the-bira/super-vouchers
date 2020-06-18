88620075cd346ead98e4cfda2b6ecd1e59f3d46d
Aimberê Rocha <aimbererm@gmail.com>
2020-06-18 03:48:18 -0300

    Adicionando schemas Yup nos Controllers Establishment e Voucher

--- src/app/controllers/EstablishmentController.js
+++ src/app/controllers/EstablishmentController.js
@@ -4,7 +4,37 @@ import Establishment from '../models/Establishment';
 
 class EstablishmentController {
   async store(req, res) {
-    return res.json(req);
+    const schema = Yup.object().shape({});
+
+    if (!(await schema.isValid())) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const {
+      id,
+      name,
+      phone,
+      description,
+      cnpj,
+      address,
+      neighborhood,
+      locality,
+      complement,
+      number,
+    } = await Establishment.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      phone,
+      description,
+      cnpj,
+      address,
+      neighborhood,
+      locality,
+      complement,
+      number,
+    });
   }
 }
 
--- src/app/controllers/VoucherController.js
+++ src/app/controllers/VoucherController.js
@@ -1,3 +1,33 @@
-class VoucherController {}
+import * as Yup from 'yup';
+
+import Voucher from '../models/Voucher';
+
+class VoucherController {
+  async store(req, res) {
+    const schema = Yup.object().shape({});
+
+    if (!(await schema.isValid())) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const {
+      id,
+      title,
+      rules,
+      validity,
+      price,
+      regularPrice,
+    } = await Voucher.create(req.body);
+
+    return res.json({
+      id,
+      title,
+      rules,
+      validity,
+      price,
+      regularPrice,
+    });
+  }
+}
 
 export default new VoucherController();
