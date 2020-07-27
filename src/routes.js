c343be13b63f6bf8318932e689db90b18159bf4a
Aimberê Rocha <aimbererm@gmail.com>
2020-07-27 17:39:56 -0300

    Finalizando umas parada

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,19 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import EstablishmentController from './app/controllers/EstablishmentController';
+import VoucherController from './app/controllers/VoucherController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/establishments', EstablishmentController.store);
+routes.post('/vouchers', VoucherController.store);
 
 export default routes;
