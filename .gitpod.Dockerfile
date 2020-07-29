f2f0106d6f20d1e5d1356119078e8bd2988595ca
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2020-07-29 15:27:14 +0000

    Atualizando dockerfiles

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,5 +1,7 @@
 FROM gitpod/workspace-postgres
 
+USER postgres
+
 # Install custom tools, runtimes, etc.
 # For example "bastet", a command-line tetris clone:
 # RUN brew install bastet
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,7 +3,7 @@ import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
